4a7f154311a4287a3ad6f29ccd318f335238f0bc
jwshin <kianms10310@gmail.com>
2020-01-30 20:14:22 +0900

    2020_0130-chromedriver path /libnative/chromedriver

--- Hello.py
+++ Hello.py
@@ -1,1 +1,14 @@
-print("Hello World")
+from selenium import webdriver
+from selenium.webdriver.common.keys import  Keys
+
+driver = webdriver.Chrome("./libnative/chromedriver")
+driver.get('http://www.naver.com')
+
+driver.quit()
+
+
+
+
+
+
+
